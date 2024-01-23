e1bd96a6a3690560fa4cc78017d3742ef3defa5f
ZettaiAzen <jolin.wong112@gmail.com>
2024-01-24 00:17:17 +0800

    added diff sfx for singleplayer and multiplayer in the main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,6 +8,8 @@ public class Menu : MonoBehaviour
 
     AudioSource m_audioSource;
 
+    [SerializeField] AudioClip[] m_audioClips;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,19 @@ public class Menu : MonoBehaviour
     {
         // playing sound
         Debug.Log("im making sound for " + sceneName);
+
+        // checking which button this is and playing the right sound
+        switch (sceneName)
+        {
+            case "SinglePlayer":
+                m_audioSource.clip = m_audioClips[0];
+                break;
+
+            case "Multiplayer_Launcher":
+                m_audioSource.clip = m_audioClips[1];
+                break;
+        }
+
         m_audioSource.Play();
         // wait for audio source to finish playing
         yield return new WaitForSeconds(m_audioSource.clip.length);
